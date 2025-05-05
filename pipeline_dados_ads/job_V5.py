4dfd0c475b77e9211151de16fb3640e5ebe24653
Diego_Araujo <131995211+DiegoCamaraDev@users.noreply.github.com>
2025-05-05 15:56:59 -0300

    melhorar a ocupação de memoria
    arredondar os resultados da coluna margem_lucro para melhor ocupação de memória

---
+++ pipeline_dados_ads/job_V5.py
@@ -0,0 +1,43 @@
+import csv
+import sqlite3
+#criar funcao para remover ponto da coluna preco_medio - utilizadi na funcao remover_ponto com o replace
+def remover_ponto(valor):
+    return int ((valor.replace('.', '')))
+#abre os arquivos CSV com os dados de produção de alimentos
+with open('producao_alimentos.csv', 'r') as file:
+    #criar leitor csv
+    reader = csv.reader(file)
+    #pular primeira linha
+    next(reader)
+
+    #conectar com o db
+    conn= sqlite3.connect('dsadb.db')
+
+    #deleta a tabela existente se houver
+    conn.execute('DROP TABLE IF EXISTS producao')
+
+    #criar uma nova tabela
+    conn.execute('''CREATE TABLE producao (
+                produto TEXT,
+                quantidade INTEGER,
+                preco_medio REAL,
+                receita_total INTEGER,
+                margem_lucro REAL
+            )''')
+    #inserir cada linha do aquivo onde a quantidade for maior que 10
+    for row in reader:
+        if int(row[1]) > 10:               
+
+            #remove o ponto da coluna preco_medio e transforma em inteiro
+            row[3] = remover_ponto(row[3])
+
+            #calcular a margem de lucro e arrendondar para melhor ocupação de memória
+            margem_lucro =round((row[3] / float(row[1])) - float(row[2]), 2)  
+
+            #inserir o registro no banco de dados
+            conn.execute('INSERT INTO producao (produto, quantidade, preco_medio, receita_total, margem_lucro) VALUES (?, ?, ?, ?, ?)', (row[0], row[1], row[2], row[3], margem_lucro))
+
+    conn.commit()
+    conn.close()
+
+    print("Job executado com sucesso!") 
